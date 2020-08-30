82eed2825c451b607e371b1bd572fb74fa0ce718
juangpu <juangabriel.urrutia@outlook.com>
2020-08-30 12:38:46 -0300

    Alterando os arquivos inde.ejs e index.js (JS Front) fazendo interações com o Usuario (prompt, alert, confim)

--- JS-I-projeto/public/javascripts/index.js
+++ JS-I-projeto/public/javascripts/index.js
@@ -1,1 +1,12 @@
-console.log("Olá Mundo!");
+// alert('Olá');
+
+// let confirmacao = confirm("Deseja realmente excluir essas informações?");
+
+// console.log(confirmacao);
+
+let nome = prompt("Digite seu nome: ");
+
+alert('Olá, ' + nome);
+
+document.querySelector("h1").innerText += ' ' + nome;
+document.querySelector("p").innerText += ' ' + nome;
